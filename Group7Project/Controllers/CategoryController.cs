bab4641615c06c4422c4335feec72e2a1e577fdc
AKO PRECIOUS <akoprecious58@gmail.com>
2023-07-14 16:23:04 -0400

    Creating a new category in the database

--- Group7Project/Controllers/CategoryController.cs
+++ Group7Project/Controllers/CategoryController.cs
@@ -17,5 +17,60 @@ namespace Group7Project.Controllers
             IEnumerable<Category> category = _dbContext.Categories;
             return View(category);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder and Name can not be the same");
+            }
+            if(ModelState.IsValid) { 
+                _dbContext.Categories.Add(obj);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder and Name can not be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(obj);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
