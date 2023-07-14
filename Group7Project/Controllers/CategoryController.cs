dbfd7b11fad0c424dfb38df77ae350d964c14541
AKO PRECIOUS <akoprecious58@gmail.com>
2023-07-14 12:50:46 -0400

    Using a costom made bootstrap style

--- Group7Project/Controllers/CategoryController.cs
+++ Group7Project/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Group7Project.DataB;
+using Group7Project.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Group7Project.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDBContext _dbContext;
+
+        public CategoryController(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> category = _dbContext.Categories;
+            return View(category);
         }
     }
 }
--- Group7Project/Program.cs
+++ Group7Project/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(
    builder.Configuration.GetConnectionString("DefualtConnection")
     ));
